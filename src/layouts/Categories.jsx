b416b9f974d194793aa7007e8f8657fefff04190
UurZer <46075903+UurZer@users.noreply.github.com>
2021-06-22 12:10:33 +0300

    Layouts - Services - ProductList

---
+++ src/layouts/Categories.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+export default function Categories() {
+  return (
+    <Menu pointing vertical>
+      <Menu.Item
+        name="home"
+
+      />
+      <Menu.Item
+        name="messages"
+
+      />
+      <Menu.Item
+        name="friends"
+
+      />
+    </Menu>
+  );
+}
